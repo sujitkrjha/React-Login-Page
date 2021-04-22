c9d8f6093002ea96dd1a8eef7879dfb861492d4d
sujitkrjha <sujitjha99@gmail.com>
2021-04-22 21:53:38 +0530

    logout session

--- react-login-app/src/Dashboard.js
+++ react-login-app/src/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getToken, removeUserSession, setUserSession } from './Utils/Common';
  
 function Dashboard(props) {
